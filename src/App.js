73a84c173ebab83b0c5191b9373cf5de9ba005e1
Bob_chemist <bob_chemist@mail.ru>
2019-04-06 16:20:33 +0300

    componentDidMount method used to get list of persons

--- src/App.js
+++ src/App.js
@@ -6,18 +6,44 @@ import PersonDetails from './Components/PersonDetails';
 import classes from './App.module.sass';
 
 class App extends Component {
+  state = {
+    showRandomPlanet: true,
+    selectedPerson: null,
+  };
+
+  toggleRandomPlanet = () => {
+    this.setState(state => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet,
+      };
+    });
+  };
+
+  onPersonSelected = id => {
+    this.setState({
+      selectedPerson: id,
+    });
+  };
+
   render() {
     return (
       <div className={classes.App}>
         <Header />
-        <RandomPlanet />
+        {this.state.showRandomPlanet ? <RandomPlanet /> : null}
+
+        <button
+          className={`${classes.TogglePlanet} btn btn-warning btn-lg`}
+          onClick={this.toggleRandomPlanet}
+        >
+          Toggle Random Planet
+        </button>
 
         <div className="row mb2">
           <div className="col-md-6">
-            <ItemList />
+            <ItemList onItemSelected={this.onPersonSelected} />
           </div>
           <div className="col-md-6">
-            <PersonDetails />
+            <PersonDetails personId={this.state.selectedPerson} />
           </div>
         </div>
       </div>
--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
 import classes from './ItemList.module.sass';
+import StarWarAPI from '../../services/sw-service';
+import Loader from '../Loader';
 
 export default class ItemList extends Component {
+  swapiService = new StarWarAPI();
+
+  state = {
+    peopleList: null,
+  };
+
+  componentDidMount() {
+    this.swapiService
+      .getAllPeople()
+      .then(peopleList => {
+        this.setState({
+          peopleList,
+        });
+      })
+      .catch();
+  }
+
+  renderItems(arr) {
+    return arr.map(({ id, name }) => {
+      return (
+        <li
+          key={id}
+          className={`list-group-item ${classes.ListGroupItem}`}
+          onClick={() => this.props.onItemSelected(id)}
+        >
+          {name}
+        </li>
+      );
+    });
+  }
+
   render() {
+    const { peopleList } = this.state;
+    if (!peopleList) return <Loader />;
     return (
       <ul className={`${classes.ItemList} list-group`}>
-        <li className={`list-group-item ${classes.ListGroupItem}`}>
-          Luke Skywalker
-        </li>
-        <li className={`list-group-item ${classes.ListGroupItem}`}>
-          Darth Vader
-        </li>
-        <li className={`list-group-item ${classes.ListGroupItem}`}>R2-D2</li>
+        {this.renderItems(peopleList)}
       </ul>
     );
   }
